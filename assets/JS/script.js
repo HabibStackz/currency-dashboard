46d06eb708f2f1786e6999300188a0a29bad49dd
Greenie115 <dggreen545@gmail.com>
2023-11-30 20:09:54 +0000

    added error handler with id's

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,16 +7,22 @@ var historicalDataQueryURL = ''
 var historicalAPIKey = ''
 
 
-var userAmount = document.getElementsByClassName('') //This should be a class for both the user input and the appended amount on the dashboard. 
-var userDate = document.getElementById('') //This is the date chosen by the user
+var userAmount = document.getElementById('userAmount') //This should be a class for both the user input and the appended amount on the dashboard. 
+var userDate = document.getElementById('userDate') //This is the date chosen by the user
 
 var selectedCurrency = document.getElementById('') //Dropdown menu for the currencies 
 
 var exchangeCards = document.getElementsByClassName('')
-
+var convertBtn = document.getElementById('convertBtn')
 
 // Display nothing if the user has not entered a value
-if (!userAmount || !userDate){
-     exchangeCards.setAttribute('class', 'hide')
-     
-}
+convertBtn.addEventListener('click', errorMessage)
+
+function errorMessage(){
+    if (userDate.value === '' || userAmount.value === ''){
+    // exchangeCards.setAttribute('class', 'hide')
+    console.log('Please enter both')
+} else{
+   return userDate.value, userAmount.value
+} }
+
