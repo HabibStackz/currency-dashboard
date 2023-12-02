2b5a3dcbb6ee11e8698b3f948ac8a74c1188e578
Abdul Rehman <abdulr568programming@outlook.com>
2023-12-02 18:43:49 +0000

    Compare value of currencies

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let currency = "AED";
+let marketdataAPIkey = "0316a175ab7d4cb592969c346cdc3a57";
 
 fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data.rates);
     })
     .catch(error => {
-        console.error(error);
+    console.error(error);
     });
+
+function fetchData() {
+    fetch(`https://openexchangerates.org/api/historical/2022-01-01.json?app_id=${marketdataAPIkey}&base=USD&symbols=EUR`)
+    .then(response => response.json())
+    .then(data => console.log(data.rates));
+}
+fetchData();
