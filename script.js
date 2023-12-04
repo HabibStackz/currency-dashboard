e3d2b11a6b76c928f65a3de96f1365ad50a9c351
Abdul Rehman <abdulr568programming@outlook.com>
2023-12-04 17:01:15 +0000

    Moved code into renamed functions

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 let currency = "AED";
 let marketdataAPIkey = "0316a175ab7d4cb592969c346cdc3a57";
 
-fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
-    .then(response => response.json())
-    .then(data => {
-    })
-    .catch(error => {
-    console.error(error);
-    });
-
-function fetchData() {
-    fetch(`https://openexchangerates.org/api/historical/2022-01-01.json?app_id=${marketdataAPIkey}&base=USD&symbols=EUR`)
-    .then(response => response.json())
-    .then(data => console.log(data.rates));
+function getCurrentCurrencyData () {
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
+        .then(response => response.json())
+        .then(data => { })
+        .catch(error => { console.error(error) });
 }
-fetchData();
+
+function getHistoricalCurrencyData() {
+    fetch(`https://openexchangerates.org/api/historical/2016-01-01.json?app_id=${marketdataAPIkey}&base=USD&symbols=EUR`)
+    .then(response => response.json())
+    .then(data => {})
+    .catch(error => { console.error(error) });
+}
+
+getCurrentCurrencyData();
+getHistoricalCurrencyData();
