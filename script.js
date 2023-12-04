07956ea8fc2af9f3ba58c179c0d4a342f0dfd525
Abdul Rehman <abdulr568programming@outlook.com>
2023-12-04 17:08:48 +0000

    Get currency codes from currency name

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ let marketdataAPIkey = "0316a175ab7d4cb592969c346cdc3a57";
 function getCurrentCurrencyData () {
     fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
         .then(response => response.json())
-        .then(data => { })
+        .then(data => {})
         .catch(error => { console.error(error) });
 }
 
-function getHistoricalCurrencyData() {
+function getHistoricalCurrencyData () {
     fetch(`https://openexchangerates.org/api/historical/2016-01-01.json?app_id=${marketdataAPIkey}&base=USD&symbols=EUR`)
     .then(response => response.json())
     .then(data => {})
     .catch(error => { console.error(error) });
 }
 
+function getCurrencyCodesFromCurrencyName () {
+    fetch('https://openexchangerates.org/api/currencies.json')
+        .then(response => response.json())
+        .then(data => {})
+    .catch(error => console.error(error));
+}
+
 getCurrentCurrencyData();
 getHistoricalCurrencyData();
+getCurrencyCodesFromCurrencyName();
