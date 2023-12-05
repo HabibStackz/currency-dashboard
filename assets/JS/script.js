5d8294f3832a104b5b844d58f69c6df408f967a6
raplordrds <habib.hakeem@yahoo.com>
2023-12-05 19:42:09 +0000

    added script to hide welcome message and show landing page with cards

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -39,3 +39,13 @@ getCurrentCurrencyData();
 getHistoricalCurrencyData();
 getCurrencyCodesFromCurrencyName();
 getCurrencyCodesFromCountryName();
+
+let currentDay = document.getElementById("currentDate");
+let startButton = document.getElementById("startExchange");
+let welcomeScreen = document.getElementById("welcomeMessage");
+
+function hideWelcomeMessage(){
+    welcomeScreen.style.display = 'none';
+}
+startButton.addEventListener('click', hideWelcomeMessage);
+
