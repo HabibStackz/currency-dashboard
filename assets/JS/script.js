295f1fca66efaf302750f9ee589b21f4c9329874
Abdul Rehman <abdulr568programming@outlook.com>
2023-12-05 08:16:13 +0000

    Loop through previous years

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,7 @@
 let currency = "AED";
 let marketdataAPIkey = "0316a175ab7d4cb592969c346cdc3a57";
+let date = new Date();
+let year = date.getFullYear();
 
 function getCurrentCurrencyData () {
     fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
@@ -9,10 +11,12 @@ function getCurrentCurrencyData () {
 }
 
 function getHistoricalCurrencyData () {
-    fetch(`https://openexchangerates.org/api/historical/2016-01-01.json?app_id=${marketdataAPIkey}&base=USD&symbols=EUR`)
-    .then(response => response.json())
-    .then(data => {})
-    .catch(error => { console.error(error) });
+    for (let i = year - 1; i > year - 6; i--) {
+        fetch(`https://openexchangerates.org/api/historical/${i}-01-01.json?app_id=${marketdataAPIkey}&base=USD&symbols=EUR`)
+        .then(response => response.json())
+        .then(data => {console.log(data)})
+        .catch(error => { console.error(error) });
+    }
 }
 
 function getCurrencyCodesFromCurrencyName () {
