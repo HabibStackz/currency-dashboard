fd8d456a7b6f7333578a671f7fb33c9dd810e93d
Abdul Rehman <abdulr568programming@outlook.com>
2023-12-07 22:25:43 +0000

    Added text explaining why the code isn't working

--- assets/JS/logic.js
+++ assets/JS/logic.js
@@ -119,3 +119,33 @@ function conversion() {
     }
 }
 // ===============================================
+
+
+// The API was working at the begining of the project. However, it has stopped working now
+// What we were hoping to do:
+/*
+    Run the getCurrentCurrencyData(currency) function passing in the currency entered by the user as the argument
+    Use the rates object inside the object fetched from the API to get all of the different rates
+    Find the correct rate which the user wanted to convert the currency to
+    Multiply the amount of money entered by the user and the rate of the correct currency
+    Display this value in the box currencyB
+*/
+/*
+    We were also hoping to create a graph displaying the history of the currency over the past few years
+    Although we have created the graph, we have not been able to display it correctly due to the API not working
+*/
+/*
+    We also wanted to write down the currency flunctuation over the year but were not able to begin this part of the program as we were trying to fix the API
+*/
+/*
+    We also wanted to append the recently searched items to the bottom of the page and store them in Local Storage. We wanted to display the 3 most recent searches on the page. We were not able to begin this part of the program as we were trying to fix the API.
+*/
+/*
+    We also wanted to allow the user to use any currency in the API we are using by:
+        typing the currency name
+        typing the currency abbreviation
+        typing the name of a country which uses that currency as its main currency
+*/
+/*
+    The reason we were unable to get this done is because the API we were using stopped working close to the end of the project, and we only realsied a few hours before submission time that is the reason the program stopped working
+*/
